dea13ce898f37de3866f23668d14aaf3d82fc961
Thai Tran <Thai.Tran@lincolnagritech.co.nz>
2025-06-13 12:57:54 +1200

    ver 1.1

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def split():
     return send_file(files[0], as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
